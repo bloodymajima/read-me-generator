6c5b7d2c9273af34d1c56faa06f0da4a4bf28c5c
bloodymajima <mdeavers18@gmail.com>
2023-11-01 22:06:38 -0400

    Updated index.js file

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,63 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require('inquirer');
+const fs = require('fs')
+const generateMarkdown = require('./utils/generateMarkdown')
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [{
+    type: 'input',
+    message: 'What is the name of your project?',
+    name: 'title'
+
+},
+{
+    type: 'input',
+    message: 'Describe your project',
+    name: "description",
+},
+{
+    type: 'input',
+    message: 'How do you use this application?',
+    name: 'Usage_info',
+},
+{
+    type: 'input',
+    message: 'Add your contributors',
+    name: 'contributors',
+},
+{
+    type: 'input',
+    message: 'Add any test instructions',
+    name: 'Test',
+},
+{
+    type: 'input',
+    message: 'What is your license?',
+    name: "license",
+},
+{
+    type: 'input',
+    message: 'Enter Github username',
+    name: 'github_username',
+},
+{
+    type: 'input',
+    message: 'Enter email address',
+    name: 'Email_address',
+}
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFileSync (fileName,generateMarkdown(data))
+ }
 
 // TODO: Create a function to initialize app
-function init() {}
-
+function init() {
+    inquirer
+        .prompt(questions)
+        .then((response) => {
+writeToFile("README.md", response)
+        })
+}
 // Function call to initialize app
 init();
